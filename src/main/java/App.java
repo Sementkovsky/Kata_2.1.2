1ceddc8062b9425bd5eb5f28bab00f847a4ca8c3
Alexey Sementkovskiy <sementkovsky@yandex.ru>
2022-09-29 15:18:44 +0300

    mySolution 1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,15 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld anotherBean =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(anotherBean.getMessage());
+        System.out.println(bean == anotherBean);
+
+        Cat cat = (Cat) applicationContext.getBean("cat");
+        System.out.println(cat.getMessage());
+        Cat anotherCat = (Cat) applicationContext.getBean("cat");
+        System.out.println(anotherCat.getMessage());
+        System.out.println(cat == anotherCat);
     }
 }
